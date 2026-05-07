113823bc72c366cc6fa9f02a908f44c27a981c67
cc24121 <clara_asantos@outlook.com>
2026-05-07 09:31:42 -0300

    Start spectra
    Initiating SpectraAI programming.

---
+++ ai/spectra/label_sets.py
@@ -0,0 +1,39 @@
+OBJECT_LABELS = [
+    "person",
+    "car",
+    "animal",
+    "door",
+    "window",
+    "table",
+    "chair",
+    "phone",
+    "book",
+    "weapon",
+]
+
+ACTION_LABELS = [
+    "running",
+    "walking",
+    "talking",
+    "sitting",
+    "standing",
+    "fighting",
+    "looking",
+    "holding",
+    "falling",
+]
+
+SCENARIO_LABELS = [
+    "street",
+    "room",
+    "forest",
+    "school",
+    "city",
+    "night",
+    "day",
+    "indoor",
+    "outdoor",
+    "dark_place",
+]
+
+SPECTRA_LABELS = OBJECT_LABELS + ACTION_LABELS + SCENARIO_LABELS
